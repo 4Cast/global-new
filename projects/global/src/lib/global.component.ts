afaaafc7ec6c7d20cb0f19f742fc5b562901c61d
Chris Curnow <chris@4cast.com.au>
2020-07-14 14:45:31 +1000

    Initial Commit

---
+++ projects/global/src/lib/global.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-global',
+  template: `
+    <p>
+      global works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class GlobalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
